f8adcaa96cb4bdf388fb4467dc05eb17801fdd87
Adam Riggs <adambolan31@gmail.com>
2023-04-25 11:51:53 -0500

    auth page edits

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -32,7 +32,7 @@ export const Login = () => {
         <main className="container--login">
             <section>
                 <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Kandy Korner</h1>
+                    <h1>Auto Shops Made Easy</h1>
                     <h2>Please sign in</h2>
                     <fieldset>
                         <label htmlFor="inputEmail"> Email address </label>
