123745de596881291acaaf97c1aa8379ba246648
Adam Riggs <adambolan31@gmail.com>
2023-04-27 10:56:50 -0500

    added store owner profile page

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -10,10 +10,10 @@ export const NavBar = () => {
                 <Link className="navbar__link" to="/profile">Profile</Link>
             </li>
             {
-                localStorage.getItem("honey_user")
+                localStorage.getItem("auto_user")
                     ? <li className="navbar__item navbar__logout">
                         <Link className="navbar__link" to="" onClick={() => {
-                            localStorage.removeItem("honey_user")
+                            localStorage.removeItem("auto_user")
                             navigate("/", {replace: true})
                         }}>Logout</Link>
                     </li>
--- src/components/profile/StoreProfile.js
+++ src/components/profile/StoreProfile.js
@@ -1,3 +1,45 @@
+  //import useEffect and state components 
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+//import appropriate image tag
+
 export const StoreProfile = () => {
-    return <></>
+    //use a fetch call that will get all of the information for the specific user
+    //that is currently logged in
+    const [profile, setProfile] = useState({
+        name: "",
+        img: ""
+    })
+    const localAutoUser = localStorage.getItem("auto_user")
+    const autoUserObject = JSON.parse(localAutoUser)
+
+    const navigate=useNavigate()
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/users/${autoUserObject.id}`)
+                .then(response => response.json())
+                .then((data) => {
+                    setProfile(data)
+                })
+        },
+        []
+    )
+
+    return <article>
+        <section>
+        <h2>Store Owner Profile</h2>
+        <div>{profile.name}</div>
+    </section>
+
+            <section>
+                <button onClick={() => navigate("/profile/UserProfileEdit")}>Edit Profile</button>
+                <button onClick={() => navigate("/vehicles/AddVehicleForm")}>Add Vehicle</button>
+                <button onClick={() => navigate("/vehicles/VehicleList")}>View Vehicles</button>
+                <button onClick={() => navigate("")}>Reviews</button>
+                <button onClick={() => navigate("")}>Edit Store Page</button>
+            </section>
+
+            </article>
+            
 }
