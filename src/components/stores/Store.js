b823eeeae9165a4537e394767912a8f964606ed2
Adam Riggs <adambolan31@gmail.com>
2023-05-05 16:00:22 -0500

    beginning css

--- src/components/stores/Store.js
+++ src/components/stores/Store.js
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom'
+import "./Store.css"
 
 export const Store = ({ store }) => {
 
-    return <section className="store" key={`customer--${store.id}`}>
+    return <section className="single-store" key={`store--${store.id}`}>
         <Link className="store_info_link" to={`/stores/StorePage/${store.id}`}>{store.name}</Link>
         <div>{store.address}</div>
 
