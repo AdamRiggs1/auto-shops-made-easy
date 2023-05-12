e3a17c9a6dcb4c04637d77ec9a9a4d895587fc3d
Adam Riggs <adambolan31@gmail.com>
2023-05-12 01:05:59 -0500

    added store list things

--- src/components/stores/Store.js
+++ src/components/stores/Store.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom'
-import "./Store.css"
+//import "./Store.css"
+import "./StoreList.css"
 
 export const Store = ({ store }) => {
 
     return <section className="single-store" key={`store--${store.id}`}>
         <Link className="store_info_link" to={`/stores/StorePage/${store.id}`}>{store.name}</Link>
-        <div>{store.address}</div>
+        <div className="store_list_address">{store.address}</div>
+        <img className="store_list_img" src={store.img} />
 
     </section>
 }
